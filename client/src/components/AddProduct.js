8af12365be2ec0813eb74fb6f1f105630651dbb2
Elaine Vuong <elaine.yt.vuong@gmail.com>
2022-06-06 15:05:37 -0400

    Modified Extendable Component Form

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import ProductInput from "./ProductInput";
 
-
-const AddProduct = ({product, productid, price, priceid, quantity, quantityid}) => {
+// toggleButtonTitle => Edit/Add
+// actionTitle => Update/Add
+// handleActionClicked => event handler
+const ToggleableForm = ({formInfo}) => {
   const [ showAddForm, setShowAddForm ] = useState(false)
 
-  const populateForm = [
-    { title: "Product Name", id: "product-name"},
-    { title: "Price", id: "product-price"},
-    { title: "Quantity", id: "product-quantity"}
-  ]
+  const {
+    product, productId,
+    price, priceId,
+    quantity, quantityId,
+    actionTitle,
+    toggleButtonTitle,
+    handleActionClick,
+  } = formInfo;
+
 
   if (showAddForm) {
     return ( 
@@ -17,11 +23,11 @@ const AddProduct = ({product, productid, price, priceid, quantity, quantityid}) 
         <p><a className="button add-product-button">Add A Product</a></p>
         <h3>Add Product</h3>
         <form>
-          <ProductInput product={populateForm[0]} />
-          <ProductInput product={populateForm[1]} />
-          <ProductInput product={populateForm[2]} />
+          <ProductInput product={{ product: { product, productId } }} />
+          <ProductInput product={{ product: { price, priceId } }} />
+          <ProductInput product={{ product: { quantity, quantityId } }} />
           <div className="actions form-actions">
-            <a className="button">Add</a>
+            <a className="button" onClick={handleActionClick}>{actionTitle}</a>
             <a 
               className="button"
               onClick={() => setShowAddForm(false)}
@@ -34,15 +40,27 @@ const AddProduct = ({product, productid, price, priceid, quantity, quantityid}) 
     return (
       <p>
         <a 
-          class="button add-product-button"
+          className="button add-product-button"
           onClick={() => setShowAddForm(true)}>
-        Add A Product</a>
+        {toggleButtonTitle}</a>
       </p>
     )
-
   }
+  
+}
 
-
+const AddProduct = () => {
+  const formInfo = {
+     product: "Product Name", productId: "product-name",
+     price: "Price", priceId: "product-price",
+     quantity: "Quantity", quantityId: "product-quantity",
+     actionTitle: "Add",
+    toggleButtonTitle: "Add a Product",
+    handleActionClick: () => { console.log('Added a product') },
+  }
+  return (
+    <ToggleableForm formInfo={formInfo}/>
+  )
 }
  
 export default AddProduct;
